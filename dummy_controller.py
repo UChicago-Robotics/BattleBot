4f49727bc5b678891ba6cac1ffb3dfd5cbba2888
Gideon Mitchell <gideonm@uchicago.edu>
2023-02-20 19:04:27 -0600

    Add heartbeat saftey feature

---
+++ dummy_controller.py
@@ -0,0 +1,26 @@
+import json
+import zmq
+import json
+
+ip = "192.168.8.232"
+port = 5555
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(f"tcp://{ip}:{port}")
+print(f"Connected to {ip} {port}")
+
+
+controls = {
+    "left_stick_y": 0,
+    "right_stick_y": 0,
+    "left_trigger": 0,
+    "right_trigger": 0
+}
+
+while True:
+    controls_json = json.dumps(controls)
+    socket.send_string(controls_json)
+
+    message = socket.recv_string()
+    print(f"Server replied: {message}\n")
--- robot_controller.py
+++ robot_controller.py
@@ -1,13 +1,12 @@
 import zmq
 import json
-import signal
+import sys
 
 from serial import Serial
-from roboclaw import Roboclaw
 from time import sleep, perf_counter
 from math import copysign
 from typing import Tuple
-from threading import Thread
+from threading import Thread, Timer
 
 def clamp(mn, mx, n): return min(max(n, mn), mx)
 
@@ -44,11 +43,11 @@ class RobotController:
         self.socket = context.socket(zmq.REP)
         self.socket.bind(f"tcp://{host}:{port}")
         
-        serial_kick = Serial('/dev/ttyS1', 38400)
-        serial_wheels = Serial('/dev/ttyUSB0', 38400)
+        # serial_kick = Serial('/dev/ttyS1', 38400)
+        # serial_wheels = Serial('/dev/ttyUSB0', 38400)
 
-        self.rclaw_kick = Roboclaw(serial_kick)
-        self.rclaw_wheels = Roboclaw(serial_wheels)
+        #self.rclaw_kick = Roboclaw(serial_kick)
+        #self.rclaw_wheels = Roboclaw(serial_wheels)
         
         self.prev_command = None
         # Prev wheels speed (python_roboclaw had some issues about reporting speeds)
@@ -58,18 +57,24 @@ class RobotController:
         # Last frame time
         self.prev_time = 0.0 # seconds
 
-    def execute(self, controller_state: str):
-        cs = controller_state.replace("\\", "").strip("\"")
-        cjson = {k: int(v) for (k, v) in dict(json.loads(cs)).items()}
+        # check when most recent heartbeat packet was received, terminate if 
+        # it has been more than 1 second without a packet
+        self.heartbeat_time = 0.0 # time until last heartbeat
+        self.heart_attack_threshold = 10.0 # latency after which robot will shut down
+        self.dead = False
         
-        # TODO these need to be -1.0 to 1.0 range
+        Timer(0.5, self.heartbeat).start()
+
+    def execute(self, cjson: json):
+
         right_stick = cjson["right_stick_y"]
         left_stick = cjson["left_stick_y"]
         right_trigger = cjson["right_trigger"]
         left_trigger = cjson["left_trigger"]
 
-        if self.prev_command == None: self.prev_command = cjson
-
+        # this code is all paurticular to the roboclaw motordriver API,
+        # so it needs to be changed
+        """
         # Differential driving
         delta = perf_counter() - self.prev_time # seconds
 
@@ -94,34 +99,52 @@ class RobotController:
            rclaw_kick_target = min(64 + 64 * (left_trigger - right_trigger), 127)
            self.rclaw_kick.forward_backward_m1(rclaw_kick_target)
            self.rclaw_kick.forward_backward_m1(rclaw_kick_target)
+        """
 
-        self.prev_command = cjson
+        if self.prev_command == None: self.prev_command = cjson
 
+        print(cjson)
+
+    # main loo for robot controller
     def listen(self):
         self.prev_time = perf_counter()
 
-        while True:
-            controller_state = self.socket.recv_string()
-            controller_state.replace("\\", "")
+        try:
+            while not self.dead:
+                # load control packets into json object
+                controller_state = self.socket.recv_string()
+                cs = controller_state.replace("\\", "").strip("\"")
+                controller_json = {k: int(v) for (k, v) in dict(json.loads(cs)).items()}
 
-            self.execute(controller_state)
-            self.socket.send_string(f"Done")
+                # check for packet type
+                if controller_json["type"] == "heartbeat":
+                    self.heartbeat_time = 0.0
+                elif controller_json["tye"] == "controller":
+                    self.execute(controller_json)
+                    self.socket.send_string(f"Done")
+                    self.heartbeat_time = 0.0
 
-            self.prev_time = perf_counter()
+                self.prev_time = perf_counter()
 
+        except BaseException as e:
+            print(e)
+            self.motor_kill()
+            sys.exit()
+
+    # check the time from the last heartbeat packet, kill the robot if threshold has passed
+    def heartbeat(self):
+        if self.heartbeat_time > self.heart_attack_threshold:
+            self.motor_kill()
+            self.dead = True
+
+    # kill the robot
     def motor_kill(self):
-        self.rclaw_wheels.forward_m1(0)
-        self.rclaw_wheels.forward_m2(0)
-        self.rclaw_kick.forward_m1(0)
-        self.rclaw_kick.forward_m2(0) 
-        sleep(1)
+        print("robot is dead")
+        self.dead = True
 
 def main():
     r = RobotController("*", 5555)
-    # signal.signal(signal.SIGINT, lambda i, f: r.motor_kill())
-
     r.listen()
-    r.motor_kill()
 
 if __name__ == "__main__":
     main()
