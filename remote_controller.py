2465bdc03de4e73d6e79829513b79102f8cf518f
TangyKiwi <orange.panda.65@gmail.com>
2023-03-28 18:58:47 -0500

    Add initial dashboard code

--- remote_controller.py
+++ remote_controller.py
@@ -13,12 +13,14 @@ socket.connect(f"tcp://{ip}:{port}")
 print(f"Connected to {ip} {port}")
 
 offset_y = 64
+WIDTH = 812
+HEIGHT = 554 + offset_y
 
 vec = pg.math.Vector2
 
 # initialize pygame
 pg.init()
-#screen = pg.display.set_mode((WIDTH, HEIGHT))
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 
 pg.joystick.init()
@@ -37,6 +39,30 @@ stick_size = 20
 
 trigger_r = 0
 trigger_l = 0
+
+# white color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (200, 200, 200)
+
+# light shade of the button
+color_light = (170,170,170)
+
+# dark shade of the button
+color_dark = (100,100,100)
+
+# stores the width of the
+# screen into a variable
+width = screen.get_width()
+
+# stores the height of the
+# screen into a variable
+height = screen.get_height()
+
+font = pg.font.SysFont(None, 24)
 
 
 # game loop
@@ -97,15 +123,15 @@ try:
             draw_stick_l = vec(0, 0)
             draw_stick_l.x = stick_l.x * math.sqrt(1 - 0.5 * stick_l.y ** 2)
             draw_stick_l.y = -stick_l.y * math.sqrt(1 - 0.5 * stick_l.x ** 2)
-            # if round(draw_stick_l.length(), 1) >= deadzone_stick:
-            #     stick_l = vec(0,0)
+            if round(draw_stick_l.length(), 1) >= deadzone_stick:
+                stick_l = vec(0,0)
 
             # right stick
             draw_stick_r = vec(0, 0)
             draw_stick_r.x = stick_r.x * math.sqrt(1 - 0.5 * stick_r.y ** 2)
             draw_stick_r.y = -stick_r.y * math.sqrt(1 - 0.5 * stick_r.x ** 2)
-            # if round(draw_stick_r.length(), 1) >= deadzone_stick:
-            #     stick_r = vec(0,0)
+            if round(draw_stick_r.length(), 1) >= deadzone_stick:
+                stick_r = vec(0,0)
 
             controls = {
                 "invert_button": inverted,
@@ -116,11 +142,29 @@ try:
             }
 
             controls_json = json.dumps(controls)
-            socket.send_string(controls_json)
+            # socket.send_string(controls_json)
 
-            message = socket.recv_string()
-            print(f"Server replied: {message}\n")
+            # response = socket.recv_string()
+            # packet = response.replace("\\", "").strip('"')
+            packet = json.loads(controls_json)
+            response_json = {
+                k: v for (k, v) in dict(packet).items()
+            }
+            right_stick_gui = response_json["right_stick_y"]
+            left_stick_gui = response_json["left_stick_y"]
+            right_trigger_gui = response_json["right_trigger"]
+            left_trigger_gui = response_json["left_trigger"]
+            inverted_gui = response_json["invert_button"]
 
+            screen.fill((255, 255, 255))
+            pg.draw.rect(screen, color_dark, [width / 2, height / 2, 140, 40])
+            screen.blit(font.render(str(inverted_gui), True, BLACK), (20, 20))
+            screen.blit(font.render(str(right_stick_gui), True, BLACK), (20, 40))
+            screen.blit(font.render(str(left_stick_gui), True, BLACK), (20, 60))
+            screen.blit(font.render(str(right_trigger_gui), True, BLACK), (20, 80))
+            screen.blit(font.render(str(left_trigger_gui), True, BLACK), (20, 100))
+
+            pg.display.update()
         else:
             text = "No Device plugged in."
             print(text)
